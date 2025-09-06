950bc6b6f24035d05446684c70bfaf654d4c657b
Quang <nguyenquangpen@gmail.com>
2025-09-06 23:29:50 +0700

    create overview page ui

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from django.urls import include, path
 
 from apps.resume.view.web import resume_screen
+from apps.overview.view.web import overview
 
 urlpatterns = [
     path("resume/", resume_screen, name="resume_screen"),
+    path("overview/", overview, name="overview"),
 ]
 
 urlpatterns += [
